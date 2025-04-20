5e32d3820fa3b1314b9a249f5ba7c32e638efa3b
Manuel Quicaia <155465120+Quicaia@users.noreply.github.com>
2025-04-20 14:30:33 +0100

    segundo comite

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/**RESET O DOCUMENTO QUANDO ELE FOR CARREGADO */
+
+window.revelar = ScrollReveal({reset:true})
+
+ScrollReveal().reveal('.efeito__texto', {
+      delay: 2000 ,
+      distance: '90px'
+     
+     });
